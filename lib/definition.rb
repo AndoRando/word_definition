82c0f7236f5619b194acc0420f0aa456e20423ac
Andrew Brennwald <leopawnz@gmail.com>
2015-08-14 13:43:50 -0700

    Implement save, clear, and call all methods for definition class.

--- lib/definition.rb
+++ lib/definition.rb
@@ -1,8 +1,21 @@
 class Definition
   attr_reader(:part_of_speech, :meaning, :id)
+  @@Definitions = []
 
   define_method(:initialize) do |attr|
     @part_of_speech = attr.fetch(:part_of_speech)
     @meaning = attr.fetch(:meaning)
+  end
+
+  define_singleton_method(:all) do
+    @@Definitions
+  end
+
+  define_singleton_method(:clear) do
+    @@Definitions = []
+  end
+
+  define_method(:save) do
+    @@Definitions.push(self)
   end
 end
--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -3,15 +3,20 @@ require('definition')
 
 describe('Definition') do
   before() do
+    Definition.clear()
     @mountain_def_noun = Definition.new({:part_of_speech=> "noun", :meaning=> "a natural elevation of the earth's surface rising more or less abruptly to a summit, and attaining an altitude greater than that of a hill."})
+    @mountain_def_noun.save()
     @mountain_def_adjective = Definition.new({:part_of_speech=> "adjective", :meaning=> "living, growing, or located in the mountains."})
+    @mountain_def_adjective.save()
     @star_def_noun = Definition.new({:part_of_speech=> "noun", :meaning=> "any of the large, self-luminous, heavenly bodies, as the sun, Polaris, etc."})
+    @star_def_noun.save()
     @star_def_verb = Definition.new({:part_of_speech=> "verb", :meaning=> "to feature as a star."})
+    @star_def_verb.save()
   end
 
   describe('#part_of_speech') do
     it('returns the part of speech of the definition') do
-      expect(@mountain_def_noun.part_of_speech()).to(eq("noun"))
+      expect(@mountain_def_adjective.part_of_speech()).to(eq("adjective"))
     end
   end
 
@@ -21,26 +26,17 @@ describe('Definition') do
     end
   end
 
-  # describe('.clear') do
-  #   it('starts out empty') do
-  #
-  #   end
-  #   it('clears all definitions') do
-  #
-  #   end
-  # end
-  #
-  # describe('.all') do
-  #   it('returns all definitions') do
-  #
-  #   end
-  # end
-  #
-  # describe('#save') do
-  #   it('saves a definition') do
-  #
-  #   end
-  # end
+  describe('.clear') do
+    it('clears all definitions') do
+      expect(Definition.clear()).to(eq([]))
+    end
+  end
+
+  describe('.all') do
+    it('returns all definitions') do
+      expect(Definition.all()).to(eq([@mountain_def_noun, @mountain_def_adjective, @star_def_noun, @star_def_verb]))
+    end
+  end
   #
   # describe('#id') do
   #   it('returns a definition id') do
