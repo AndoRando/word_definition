5c9d9f4551c843034be7306ccfa69c7c87a78751
Andrew Brennwald <leopawnz@gmail.com>
2015-08-14 16:00:47 -0700

    First draft of sinatra structure.

---
+++ app.rb
@@ -0,0 +1,50 @@
+require('sinatra')
+require('sinatra/reloader')
+also_reload('lib/**/*.rb')
+require('./lib/definition')
+require('./lib/word')
+
+get('/') do
+  erb(:index)
+end
+
+get('/words/new') do
+  erb(:add_word)
+end
+
+get('/words') do
+  @words = Word.all()
+  erb(:words)
+end
+
+post('/words') do
+  word = params.fetch('word')
+  Word.new(word).save()
+  @words = Word.all()
+  erb(:success)
+end
+
+get('/definitions/:id') do
+  @definition = Definition.find(params.fetch('id').to_i())
+  erb(:definition)
+end
+
+get('words/:id') do
+  @word = Word.find(params.fetch('id').to_i())
+  erb(:word)
+end
+
+get('words/:id/definitions/new') do
+  @word = Word.find(params.fetch('id').to_i())
+  erb(:add_definition)
+end
+
+post('/definitions') do
+  part_of_speech = params.fetch('part_of_speech')
+  meaning = params.fetch('meaning')
+  @definition = Definition.new({:part_of_speech=> part_of_speech, :meaning=> meaning})
+  @definition.save()
+  @word = Word.find(params.fetch('word_id').to_i())
+  @word.add_definition(@definition)
+  erb(:success)
+end
